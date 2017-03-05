84c6cc2a3b3c0c7dfe6f647783449b7001924a8e
Venkada Narasiman Prasad <venkada.prasad@mail.utoronto.ca>
2017-03-05 03:49:53 -0500

    h

--- app/src/main/java/com/ants/DementiaPage.java
+++ app/src/main/java/com/ants/DementiaPage.java
@@ -1,4 +1,4 @@
-package com.ants.User;
+package com.ants;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
--- app/src/main/java/com/ants/MainActivity.java
+++ app/src/main/java/com/ants/MainActivity.java
@@ -1,13 +1,13 @@
 package com.ants;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.ants.Network.ResponseAction;
 import com.ants.Network.ServerController;
-import com.ants.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
 
 
     }
@@ -27,6 +29,12 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         ServerController.loginUser(this, loginAction, username, password);
+        Intent intentDementia = new Intent(this, DementiaPage.class);
+        //Intent intentCaregiver = new Intent(this, CareGiver.class);
     }
 
+    public void register (View view) {
+        Intent intent = new Intent(this, SignUpPage.class);
+        startActivity(intent);
+    }
 }
